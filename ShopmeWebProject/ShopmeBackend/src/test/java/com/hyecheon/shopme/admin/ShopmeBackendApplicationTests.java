e6aa8083b14eecb5c80382aebdaeeaff69bcb3e6
이혜천 <rainbow880616@gmail.com>
2021-08-11 14:45:48 +0900

    Initial commit

---
+++ ShopmeWebProject/ShopmeBackend/src/test/java/com/hyecheon/shopme/admin/ShopmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyecheon.shopme.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ShopmeWebProject/src/main/java/com/example/shopmewebproject/ShopmeWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopmewebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopmeWebProjectApplication.class, args);
+    }
+
+}
